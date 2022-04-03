a85802b406ec9484bf9869b4379a64b078140242
ArtyomSolovev <artyom.solovev@icloud.com>
2022-04-03 16:20:34 +0300

    Initial Commit

---
+++ cloudTag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cloudTag
+//
+//  Created by Артем Соловьев on 03.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
